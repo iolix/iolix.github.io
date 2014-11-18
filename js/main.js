e4daa10d52a83eb9ee974fb4e359831e2474e177
Ioli Xanthaki <ioli.xanthaki@gmail.com>
2014-11-18 20:10:17 +0000

    init web portfolio repository

---
+++ js/main.js
@@ -0,0 +1,167 @@
+// HOVER SETTINGS
+
+$("#menu-dixi").mouseenter(function() {
+	$("#menu-dixi img").attr("src", "pics/menu/dixi-selected.png");
+});
+
+$("#menu-dixi").mouseleave(function() {
+	$("#menu-dixi img").attr("src", "pics/menu/dixi.png");
+});
+
+$("#menu-portfolio").mouseenter(function() {
+	$("#menu-portfolio img").attr("src", "pics/menu/portfolio-selected.png");
+});
+
+$("#menu-portfolio").mouseleave(function() {
+	$("#menu-portfolio img").attr("src", "pics/menu/portfolio.png");
+});
+
+$("#menu-about").mouseenter(function() {
+	$("#menu-about img").attr("src", "pics/menu/about-selected.png");
+});
+
+$("#menu-about").mouseleave(function() {
+	$("#menu-about img").attr("src", "pics/menu/about.png");
+});
+
+// MAIN MENU SELECTIONS
+
+$("#menu-about").click(function() {
+	$("#menu-about img").attr("src", "pics/menu/about-selected.png");
+	$("header").addClass('small');
+	$("#portfolio-sections").addClass('invisible');
+	$("#body-background").removeClass('blank');
+	$("#aboutme-text").removeClass('invisible');
+	$(".covers-container").addClass('invisible');
+	$("article").addClass('hidden');
+	$("#linkedin").attr('src', 'pics/menu/linkedin.png');
+	$("#behance").attr('src', 'pics/menu/behance.png');
+	$("#email").css('color', 'white');
+});
+
+$("#menu-dixi").click(function() {
+	$("header").removeClass('small');
+	$("#portfolio-sections").addClass('invisible');
+	$("#body-background").removeClass('blank');
+	$("#aboutme-text").removeClass('invisible');
+	$(".covers-container").addClass('invisible');
+	$("article").addClass('hidden');
+	$("#linkedin").attr('src', 'pics/menu/linkedin.png');
+	$("#behance").attr('src', 'pics/menu/behance.png');
+	$("#email").css('color', 'white');
+});
+
+$("#menu-portfolio").click(function() {
+	$("header").addClass('small');
+	$("#portfolio-sections").removeClass('invisible');
+	$("#body-background").addClass('blank');
+	$("#aboutme-text").addClass('invisible');
+	$(".covers-container").removeClass('invisible');
+	$("article").addClass('hidden');
+	$("#linkedin").attr('src', 'pics/menu/linkedin.png');
+	$("#behance").attr('src', 'pics/menu/behance.png');
+	$("#email").css('color', 'white');
+});
+
+// PORTFOLIO SECTIONS ARROWS
+
+$('#next').click(function() {
+  event.preventDefault();
+  $('#covers').animate({
+    marginLeft: "-=400px"
+  }, "fast");
+});
+
+$('#previous').click(function() {
+  event.preventDefault();
+  $('#covers').animate({
+    marginLeft: "+=400px"
+  }, "fast");
+});
+
+// PORTFOLIO SECTIONS MENU
+
+$(".portfolio-section").click(function() {
+  $("article").removeClass('hidden');
+  $(".covers-container").addClass('invisible');
+  $("#linkedin").attr('src', 'pics/menu/linkedin-black.png');
+  $("#behance").attr('src', 'pics/menu/behance-black.png');
+  $("#email-link img").attr('src', 'pics/menu/email-black.png');
+  $("#email-link span").css('color', 'black');
+});
+
+// PROJECT PAGES
+
+function updateContent(page) {
+	var contentObject;
+
+	if(page == 'retail') {
+		contentObject = retail;
+		$("#pic-gallery").removeClass("horizontal");
+		$("#pic-gallery").addClass("vertical");
+	}
+	else if(page == 'web') {
+		contentObject = web;
+		$("#pic-gallery").removeClass("horizontal");
+		$("#pic-gallery").addClass("vertical");
+	}
+	else if(page == 'branding') {
+		contentObject = branding;
+		$("#pic-gallery").removeClass("horizontal");
+		$("#pic-gallery").addClass("vertical");
+	}
+	else if(page == 'infographics') {
+		contentObject = infographics;
+		$("#pic-gallery").removeClass("vertical");
+		$("#pic-gallery").addClass("horizontal");
+	}
+	else if(page == 'packaging') {
+		contentObject = packaging;
+		$("#pic-gallery").removeClass("horizontal");
+		$("#pic-gallery").addClass("vertical");
+	}
+	else if(page == 'interior') {
+		contentObject = interior;
+		$("#pic-gallery").removeClass("horizontal");
+		$("#pic-gallery").addClass("vertical");
+	}
+	else if(page == 'various') {
+		contentObject = poster;
+		$("#pic-gallery").removeClass("horizontal");
+		$("#pic-gallery").addClass("vertical");
+	}
+
+	$("#project-title").empty();
+	$("#project-intro").empty();
+	$("#project-credits").empty();
+	$("#pic-gallery").empty();
+
+	contentObject.images.forEach(function(entry) {
+		$("#pic-gallery").append('<img src="'+entry+'"/>');
+	});
+
+	$("#project-title").text(contentObject.title);
+	$("#project-intro").text(contentObject.text);
+	$("#project-credits").text(contentObject.credits);
+}
+
+// HOW IT SHOULD WORK
+
+/*
+var contentObject = updateContent.val();
+
+function updateContent() {
+	$("#project-title").empty();
+	$("#project-intro").empty();
+	$("#project-credits").empty();
+	$("#pic-gallery").empty();
+
+	$("#project-title").text(contentObject.title);
+	$("#project-intro").text(contentObject.text);
+	$("#project-credits").text(contentObject.credits);
+
+	contentObject.images.forEach(function(entry) {
+		$("#pic-gallery").append("<img src="'+entry+'"/>");
+	});
+}
+*/
---
+++ js/projects.js
@@ -0,0 +1,120 @@
+var retail = {
+	title: 'sunglass hut creative window elevations',
+	text: 'A selection of some of the creative campaigns designed for the flagships of Sunglass Hut, a world-wide sunglasses store chain managed by the renowned Italian retailer Luxottica. These projects where developed starting from the first creative proposals based on the brief, to the final printing files and production.',
+	credits: 'Credits: La Cellula Lab, Torino',
+	images: ['pics/works/Retail-SGHbetrench.jpg',
+			'pics/works/Retail-SGHneverhide.jpg',
+			'pics/works/Retail-SGHpersol.jpg',
+			'pics/works/Retail-SGHrbliteforce.jpg',
+			'pics/works/Retail-SGHtiffany.jpg',
+			'pics/works/Retail-SGHvday.jpg',
+			'pics/works/Retail-SGHxmas.jpg',
+			]
+};
+
+var web = {
+	title: 'nolita & rare websites',
+	text: 'Nolita and Rare, female and male clothing brands, needed an online shop; I was asked to design the website lay-out for each page of both websites.',
+	images: ['pics/works/Web-Nolita01.jpg',
+			'pics/works/Web-Nolita02.jpg',
+			'pics/works/Web-Rare01.jpg',
+			'pics/works/Web-Rare02.jpg'
+			]
+};
+
+var branding = {
+	title: 'qube srl logo restyling',
+	text: 'Qube is an Italian agency that deals with infrastructure design, disaster recovery systems and offers IT solutions through the development of new Java and Microsoft software. They hired me to refresh their outdated Corporate Identity. This included a new logo, business cards, letterhead, a brochure and a new lay-out for heir website.',
+	images: ['pics/works/Identity-Qube01.jpg',
+			'pics/works/Identity-Qube02.jpg',
+			'pics/works/Identity-Qube03.jpg'
+			]
+};
+
+var branding02 = { // TO INSERT
+	title: 'mechanical services brand identity',
+	text: 'Medtronic is the worlds largest medical technology company. I designed the logo, business cards, letterhead and website lay-out for Mechanical Services, the companys manufacturing sector.',
+	credits: 'Credits: La Cellula Lab, Torino',
+	images: ['pics/works/Identity-MechanicalServices01.jpg',
+			'pics/works/Identity-MechanicalServices02.jpg'
+			]
+};
+
+var infographics = {
+	title: 'infographic for the question what makes us choose a book?',
+	text: 'I designed this hypothetical infographic as an excercise during my participation at the 3-day workshop Thype! in Turin, Italy.',
+	images: ['pics/works/Infographics-Thype.jpg'
+			]
+};
+
+var packaging = {
+	title: 'various',
+	text: 'A selection of packaging design products from my collaboration with Arcs design studio in Turin, Italy. Packaging restyling for the Scottex tissues, design of the promotional glass-sleeves for Martini Cocktails, image manipulation for new Kinder products.',
+	credits: 'Credits: ARCS, Torino',
+	images: ['pics/works/Packaging-Scottex01.jpg',
+			'pics/works/Packaging-Scottex02.jpg',
+			'pics/works/Packaging-MartiniSleeves.jpg',
+			'pics/works/Packaging-KinderBrioss.jpg',
+			'pics/works/Packaging-KinderCerealCup.jpg',
+			'pics/works/Packaging-NutellaNatale.jpg'
+			]
+};
+
+var interior = {
+	title: 'big bag box space design & identity',
+	text: 'In 2010 I collaborated with the Milan-based studio Interaction Design Lab for the creation of an alternative hostel to complement the studios coworking space. The concept was to create an urban camping area where they could host their coworking visitors as well as the visitors of the very important yearly Milan exhibition Salone del Mobile. I was asked to create the logo, space signage, decoration for the tents and walls, a welcome kit and a map for the guests.',
+	images: ['pics/works/Interior-bbb01.jpg',
+			'pics/works/Interior-bbb02.jpg',
+			'pics/works/Interior-bbb03.jpg',
+			'pics/works/Interior-bbb04.jpg',
+			'pics/works/Interior-bbb05.png'
+			]
+};
+
+var interior02 = { // TO INSERT
+	title: 'skinos events',
+	text: 'missing',
+	credits: 'Credits: +minus Communications, Athens',
+	images: ['pics/works/Expo-SkinosMEC01.jpg',
+			'pics/works/Expo-SkinosMEC02.jpg',
+			'pics/works/Events-Skinos01.jpg',
+			'pics/works/Events-Skinos02.jpg'
+			]
+};
+
+var poster = {
+	title: 'patrimonio universal film poster',
+	text: 'My spanish friend Iban Gonzalez, radio host and film director, asked me to create the poster for his film Patrimonio Universal, a documentary about the Flamenco culture in the city of Birmingham, UK.',
+	images: ['pics/works/Poster-PatrimonioUniversal01.jpg',
+			'pics/works/Poster-PatrimonioUniversal02.jpg',
+			'pics/works/Poster-PatrimonioUniversal03.jpg',
+			'pics/works/Poster-PatrimonioUniversal04.jpg'
+			]
+};
+
+var booklet = { // TO INSERT
+	title: 'booklet for a trip in north wales',
+	text: 'missing',
+	images: ['pics/works/Booklet-Wales01.jpg',
+			'pics/works/Booklet-Wales02.jpg',
+			'pics/works/Booklet-Wales03.jpg'
+			]
+};
+
+var print = { // TO INSERT
+	title: 'design portfolio 09',
+	text: 'missing',
+	images: ['pics/works/Print-portfolio01.jpg',
+			'pics/works/Print-portfolio02.jpg'
+			]
+};
+
+var illustration = { // TO INSERT
+	title: 'the cyberiad illustration',
+	text: 'As a thesis for my graduation from Vakalo School of Art and Design, I chose to illustrate a book. The Cyberiad is a science fiction book by Polish author Stanislaw Lem. It  focuses on the adventures of two intelligent robots, Trurl and Klapaucius, master constructors of computers who sally about the cosmos to meet challenges. To illustrate this book, I built each character using old computers waste, photographed the characters and finally combined them with hand drawn and photoshop sketches for the final page scenes.',
+	images: ['pics/works/Illustration_Cyberiad01.jpg',
+			'pics/works/Illustration_Cyberiad02.jpg',
+			'pics/works/Illustration_Cyberiad03.jpg',
+			'pics/works/Illustration_Cyberiad04.jpg',
+			]
+};
